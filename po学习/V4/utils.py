753d73b8bd7e835c990e345bd642f97ac43f7dd5
albertzh <Password02!>
2021-04-16 18:01:03 +0800

    update——test

--- po学习/V4/utils.py
+++ po学习/V4/utils.py
@@ -1,3 +1,4 @@
+# 定义一个工具类
 import time
 
 from selenium import webdriver
@@ -7,23 +8,21 @@ from selenium.webdriver.common.by import By
 class UtilsDriver:
     _driver = None
 
-    # 类级别的方法
     @classmethod
     def get_driver(cls):
-        #判断driver是否创建
         if cls._driver is None:
             cls._driver = webdriver.Chrome()
-            cls._driver.implicitly_wait(7)
+            cls._driver.implicitly_wait(5)
+            cls._driver.get("http://localhost")
         return cls._driver
 
     @classmethod
     def quit_driver(cls):
-        if cls._driver is not None:
-            cls._driver.quit()
+        if cls._driver != None:
+            cls.get_driver.quit()
             cls._driver = None
 
-# 定义一个弹出框消息获取方法
+
 def get_msg():
     time.sleep(1)
-    driver = UtilsDriver.get_driver()
-    return driver.find_element(By.CSS_SELECTOR, '.layui-layer-content').text
+    return UtilsDriver.get_driver().find_element(By.CSS_SELECTOR, '.layui-layer-content').text
