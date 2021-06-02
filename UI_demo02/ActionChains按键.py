e69424b5d3c05ce470f5b9e75bc54d0a2488f75e
albertzh <albertzh>
2021-06-02 17:58:32 +0800

    py update

--- UI_demo02/ActionChains按键.py
+++ UI_demo02/ActionChains按键.py
@@ -1,26 +1,44 @@
 import time
 
 from selenium import webdriver
-from selenium.webdriver import ActionChains
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
-driver.get("http://sahitest.com/demo/label.htm")
+driver.maximize_window()
+
+driver.get('http://sahitest.com/demo/label.htm')
+
 input1 = driver.find_elements_by_tag_name('input')[3]
 input2 = driver.find_elements_by_tag_name('input')[4]
+
+# ----------复制-----------
 action = ActionChains(driver)
-action.click(input1).perform()
 input1.click()
+action.click(input1).perform()
+action.send_keys('Test Keys')
 time.sleep(1)
-action.send_keys('Test Keys').perform()
-time.sleep(2)
-action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).perform()
+action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL)  # ctrl+a
 time.sleep(1)
-action.key_down(Keys.CONTROL).send_keys('c').key_up(Keys.CONTROL).perform()
+action.key_down(Keys.CONTROL).send_keys('c').key_up(Keys.CONTROL)  # ctrl+c
 time.sleep(1)
-action.key_down(Keys.CONTROL, input2).send_keys('v').key_up(Keys.CONTROL).perform()
-time.sleep(2)
+action.key_down(Keys.CONTROL, input2).send_keys('v').key_up(Keys.CONTROL).perform()  # ctrl+v
+time.sleep(1)
+
+# "--------复制 -----------"
+# input1.send_keys('Test Keys')
+# time.sleep(1)
+# input1.send_keys(Keys.CONTROL, 'a')
+# time.sleep(1)
+# print(input1.send_keys(Keys.CONTROL, 'c'))
+# print(input1)
+# time.sleep(1)
+# input2.send_keys(Keys.CONTROL, 'v')
+# time.sleep(1)
+
+# -----结果-------
 print(input1.get_attribute('value'))
 print(input2.get_attribute('value'))
 driver.quit()
--- UI_demo03/test.py
+++ UI_demo03/test.py
@@ -3,14 +3,16 @@ import time
 
 print("$('.finishTime div').html('%s')" % 'xxx')
 time_struct = time.strptime('2021-04-13', '%Y-%m-%d')
-print(time.localtime().tm_hour)
+print(time.localtime())
 print(time.time())
 
 t = '2021-04-13 17:30:00'
 time_struct2 = time.strptime(t, '%Y-%m-%d %H:%M:%S')
 str_time = time.strftime('%Y年%m月%d日 %H时%M分%S秒', time.localtime())
+print(time.strftime('%Y年%m月%d日 %H时%M分%S秒'))
 print(str_time)
 
+print("datetime:", datetime.datetime.now())
 
 threeDayAgo = datetime.datetime.now() - datetime.timedelta(days=3)
 print(threeDayAgo)
